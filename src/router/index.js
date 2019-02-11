f345ae73a300f2d72ca4f8fa68cf37191b44db81
Grochulele <grochulele@gmail.com>
2019-02-11 19:14:02 +0100

    vvvv

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import List from '@/components/List'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 
 Vue.use(Router)
+Vue.use(axios, VueAxios)
 
 export default new Router({
   routes: [
@@ -10,6 +15,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'List',
+      component: List
     }
   ]
 })
